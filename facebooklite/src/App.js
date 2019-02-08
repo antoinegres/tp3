d5f4e11e1ee25abf60a8ab5f3923d1232093ec98
Antoine <>
2019-02-08 03:26:14 +0100

    switch user ok
    il manque change style, les pics, et la répartition en components

--- facebooklite/src/App.js
+++ facebooklite/src/App.js
@@ -2,36 +2,89 @@ import React, { Component } from 'react';
 import './App.css';
 import './Components/boutons.css';
 import Bouton from './Components/boutons';
+import mar from './mar.jpg';
+import fra from './fra.jpg';
 
 class App extends Component {
+    
+  constructor() {
+     super();
+     this.state = {
+       value: 0,
+       prenom: 'Prenom',
+       nom: 'Nom',
+       date: 'Date de Naissance',
+       bio: 'biographie',
+       color: '#e7e7e7',
+     }
+   }
+    
+  SuperClick = () => {
+     this.setState({
+       value: this.state.value + 1
+     });
+  }
+  
+  AntoineClick = () => {
+     this.setState({
+       prenom: 'Antoine',
+       nom: 'Grès',
+       date: '21/05/1997',
+       bio: 'Je suis Antoine !', 
+       color: '#008CBA',
+       pic: 'ant.jpg',
+         
+     });
+  }
+  
+  FranClick = () => {
+     this.setState({
+       prenom: 'François',
+       nom: 'Sabatier',
+       date: '12/04/1979',
+       bio: 'Je suis François !',
+       color: '#e7e7e7',
+       pic: 'fra.jpg',
+     });
+  }
+  
+  MarieClick = () => {
+     this.setState({
+       prenom: 'Marie',
+       nom: 'Lembrez',
+       date: '03/06/1799',
+       bio: 'Je suis Marie !', 
+       color: '#f44336',
+       pic: 'mar.jpg',
+     });
+  }
+    
   render() {
     return (
         <main>
         <header>
-        <section id="butons">
-            <button class="button button3">Marie</button>
-            <button class="button button2">François</button>
-            <button class="button button1">Antoine</button>
+        <section id="boutons">
+            <button class="button button3" onClick={this.MarieClick}>Marie</button>
+            <button class="button button2" onClick={this.FranClick}>François</button>
+            <button class="button button1" onClick={this.AntoineClick}>Antoine</button>
         </section>
         </header>
         <center>
-            <section id="profil">
+            <section id="profil" style={{backgroundColor: this.state.color }}>
                 <center>
                     <div class="roundedImage">
-                        <img src="pp.jpg" width="200" height="200"/>
+                        <img src='fra.jpg' width="200" height="200"/>
                     </div>
                 </center>
-                    <h1 class="prenom">Prénom</h1>
-                    <h1 class="nom">Nom</h1>
-                    <h1 class="date">Date de naissance</h1>
+                    <h1 class="prenom">{this.state.prenom}</h1>
+                    <h1 class="nom">{this.state.nom}</h1>
+                    <h1 class="date">{this.state.date}</h1>
                     <button class="button button4">Change Style</button>
             </section>
        
-            <section id="bio">
+            <section id="bio" style={{backgroundColor: this.state.color }}>
                 <center>
-                    <p class="bio">biographiemmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
-                    <p>mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
-                    <p>mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
+                    <p class="bio">{this.state.bio}</p>
                 </center>
                     <button class="button button5">C est super !</button>
             </section>
@@ -40,6 +93,8 @@ class App extends Component {
         
                 
             
+    
+   
         
         
     );
