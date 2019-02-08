547029a1e44b5dbbc3873b7f15ceaece38b96176
Antoine <>
2019-02-08 11:20:05 +0100

    image ok

--- facebooklite/src/App.js
+++ facebooklite/src/App.js
@@ -2,20 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 import './Components/boutons.css';
 import Bouton from './Components/boutons';
-import mar from './mar.jpg';
-import fra from './fra.jpg';
+
+//import ant from './ant.jpg';
 
 class App extends Component {
     
   constructor() {
      super();
      this.state = {
-       value: 0,
+       value: false,
        prenom: 'Prenom',
        nom: 'Nom',
        date: 'Date de Naissance',
        bio: 'biographie',
        color: '#e7e7e7',
+       pic: require('./user.jpg'),
      }
    }
     
@@ -32,7 +33,7 @@ class App extends Component {
        date: '21/05/1997',
        bio: 'Je suis Antoine !', 
        color: '#008CBA',
-       pic: 'ant.jpg',
+       pic: require('./ant.jpg'),
          
      });
   }
@@ -44,7 +45,7 @@ class App extends Component {
        date: '12/04/1979',
        bio: 'Je suis François !',
        color: '#e7e7e7',
-       pic: 'fra.jpg',
+       pic: require('./fra.jpg'),
      });
   }
   
@@ -55,9 +56,16 @@ class App extends Component {
        date: '03/06/1799',
        bio: 'Je suis Marie !', 
        color: '#f44336',
-       pic: 'mar.jpg',
+       pic: require('./mar.jpg'),
      });
   }
+  
+  ChangeClick = (e) => {
+      
+      
+      
+     }
+
     
   render() {
     return (
@@ -73,13 +81,13 @@ class App extends Component {
             <section id="profil" style={{backgroundColor: this.state.color }}>
                 <center>
                     <div class="roundedImage">
-                        <img src='fra.jpg' width="200" height="200"/>
+                        <img src={this.state.pic} width="200" height="200"/>
                     </div>
                 </center>
                     <h1 class="prenom">{this.state.prenom}</h1>
                     <h1 class="nom">{this.state.nom}</h1>
                     <h1 class="date">{this.state.date}</h1>
-                    <button class="button button4">Change Style</button>
+                    <button class="button button4" onClick={this.ChangeClick(this.state.value)}>Change Style</button>
             </section>
        
             <section id="bio" style={{backgroundColor: this.state.color }}>
