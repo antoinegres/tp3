a67f6a10a93a00ac60ee1877247e0194dd8b41ea
Antoine <>
2019-02-09 19:23:50 +0100

    facebooklite fonctionnel
    J'ai rencontrer un problème au niveau du 'npm start' pour ce fichier.
    J'ai trouvé un moyen de contourné ce problème pour finir le tp, et de mon coté le programme est fonctionnel, mais j'ai due me contenter de copier coller ce que j'ai changer sans pouvoir tester avant de push une dernière fois.
    Si vous n'arrivez pas à un résultat fonctionnel, n'hésitez pas à m'en informer pour qu'on puisse trouver une alternative.
    Merci,
    Antoine

--- facebooklite/src/App.js
+++ facebooklite/src/App.js
@@ -10,13 +10,18 @@ class App extends Component {
   constructor() {
      super();
      this.state = {
-       value: false,
+       value: 0,
        prenom: 'Prenom',
        nom: 'Nom',
        date: 'Date de Naissance',
-       bio: 'biographie',
+       bio: 'Biographie',
        color: '#e7e7e7',
+       //color: '#008CBA',     
        pic: require('./user.jpg'),
+       sup: 0,
+       supa: 0,
+       supf: 0,
+       supm: 0,
      }
    }
     
@@ -32,9 +37,9 @@ class App extends Component {
        nom: 'Grès',
        date: '21/05/1997',
        bio: 'Je suis Antoine !', 
-       color: '#008CBA',
+       //color: '#008CBA',
        pic: require('./ant.jpg'),
-         
+       sup: this.state.supa,
      });
   }
   
@@ -44,8 +49,9 @@ class App extends Component {
        nom: 'Sabatier',
        date: '12/04/1979',
        bio: 'Je suis François !',
-       color: '#e7e7e7',
+       //color: '#e7e7e7',
        pic: require('./fra.jpg'),
+       sup: this.state.supf,
      });
   }
   
@@ -55,17 +61,49 @@ class App extends Component {
        nom: 'Lembrez',
        date: '03/06/1799',
        bio: 'Je suis Marie !', 
-       color: '#f44336',
+       //color: '#f44336',
        pic: require('./mar.jpg'),
+       sup: this.state.supm,
      });
   }
   
-  ChangeClick = (e) => {
-      
-      
-      
-     }
-
+  ChangeClick = () => {
+      if (this.state.value%2==1){
+          this.setState({
+            color: '#008CBA',
+          });
+      }
+      else {
+        this.setState({
+          color: '#f44336',
+        });
+      }
+      this.setState({
+            value: this.state.value +1,
+          });
+  }
+  
+  SuperClick = () => {
+      if ( this.state.prenom == 'Antoine'){
+          this.setState({
+          supa: this.state.supa +1,
+          sup: this.state.supa,
+          });
+      }
+      else if (this.state.prenom == 'François'){
+          this.setState({
+          supf: this.state.supf +1,
+          sup: this.state.supf,
+          });
+      }
+      else if (this.state.prenom == 'Marie'){
+          this.setState({
+          supm: this.state.supm +1,
+          sup: this.state.supm,
+          });
+      }
+  }
+     
     
   render() {
     return (
@@ -78,6 +116,7 @@ class App extends Component {
         </section>
         </header>
         <center>
+            
             <section id="profil" style={{backgroundColor: this.state.color }}>
                 <center>
                     <div class="roundedImage">
@@ -87,14 +126,15 @@ class App extends Component {
                     <h1 class="prenom">{this.state.prenom}</h1>
                     <h1 class="nom">{this.state.nom}</h1>
                     <h1 class="date">{this.state.date}</h1>
-                    <button class="button button4" onClick={this.ChangeClick(this.state.value)}>Change Style</button>
+                    <button class="button button4" onClick={this.ChangeClick}>Change Style</button>
             </section>
        
             <section id="bio" style={{backgroundColor: this.state.color }}>
                 <center>
-                    <p class="bio">{this.state.bio}</p>
+                    <h1 class="bio">{this.state.bio}</h1>
+                    
                 </center>
-                    <button class="button button5">C est super !</button>
+                    <button class="button button5" onClick={this.SuperClick}>Super ! ({this.state.sup})</button>
             </section>
         </center>
         </main>
